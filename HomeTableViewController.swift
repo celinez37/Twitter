22dba1e1ab8c09b6cfa0e2a674afe4e59a5f87b4
celinez37 <cbzhao@ucsd.edu>
2021-10-12 23:41:31 -0700

    Twitter part 2
    Added twitter part 2

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -20,22 +20,23 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
         
     }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        self.loadTweet()
-//        print(tweetArray)
-//    }
-//
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+        
+    }
+    
     @objc func loadTweets(){
         
         numOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numOfTweet]
-        
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String:Any], success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             
@@ -100,6 +101,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
